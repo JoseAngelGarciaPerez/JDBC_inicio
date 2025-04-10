404bcfd6185d0f265cdc6529310f31914d27be36
José Ángel García Pérez <73956458+JoseAngelGarciaPerez@users.noreply.github.com>
2025-04-10 12:53:11 +0200

    Añadido clase Oficina con LOMBOK

---
+++ src/main/java/modelos/Oficina.java
@@ -0,0 +1,19 @@
+package modelos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Oficina {
+    private String codigoOficina;
+    private String ciudad;
+    private String pais;
+    private String region;
+    private String codigoPostal;
+    private String telefono;
+    private String lineaDireccion1;
+    private String lineaDireccion2;
+}
